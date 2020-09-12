0ba6b8a37f754f2d5a0b1aa549da3195d1c0e11e
Lianghd1997 <lianghd1997@163.com>
2020-09-12 19:46:18 +0800

    add uni-ui

---
+++ src/pages/home/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <view>
+        首页
+        <text class="iconfont icon-zhuye"></text>
+        <uni-badge text="1"></uni-badge>
+        <uni-badge text="2" type="success"></uni-badge>
+        <uni-badge text="3" type="primary" :inverted="true"></uni-badge>
+    </view>
+</template>
+
+<script>
+    import { uniBadge } from '@dcloudio/uni-ui'
+    export default {
+        name: "home",
+        components: {
+            uniBadge
+        }
+    }
+</script>
+
+<style scoped>
+
+
+</style>
---
+++ src/pages/horizontal/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <view>横屏</view>
+</template>
+
+<script>
+    export default {
+        name: "horizontal"
+    }
+</script>
+
+<style scoped>
+
+</style>
